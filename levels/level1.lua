c6326b97aa6df6b80be3a6fcf4f010a1d882dc0e
VDE - Vooperian Department of Engineers <51934493+vooperia@users.noreply.github.com>
2023-12-21 07:47:12 -0600

    Update level1.lua

--- levels/level1.lua
+++ levels/level1.lua
@@ -8,9 +8,12 @@ require("levels/Mycamera")
 
 world = bf.newWorld(0, 90.81, true)
 
-ground.new(world, 220, 1020, 650, 700, 0.06)
-wall.new(world, -50, 500, 100, 1000)
-wall.new(world, 0, -50, 5000, 100)
+
+-- world, x, y, w, h, r
+ground.new(world, 220, 1020, 650, 700, 0.06) -- start platform
+ground.new(world, 2000, 1020, 100, 100, 0.06) -- swinging platform
+wall.new(world, -50, 500, 100, 1000) -- left wall
+wall.new(world, 0, -50, 5000, 100) -- ceiling
 
 function level1:enter()
     chapter1Music:setVolume(volume)
